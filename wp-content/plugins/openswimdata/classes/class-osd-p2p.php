c968fc5ad880f2e2c4991e27683f4c326b60ebd0
Phh <phh@peytz.dk>
2013-11-16 01:17:53 +0000

    connect meetings and results

--- wp-content/plugins/openswimdata/classes/class-osd-p2p.php
+++ wp-content/plugins/openswimdata/classes/class-osd-p2p.php
@@ -6,6 +6,7 @@
  * @subpackage OpenSwimData
  */ 
 class OSD_P2P {
+	protected $_p2p = array( 'swimmers_results', 'meetings_results' );
 
 	function __construct() {
 		if ( !in_array( 'posts-to-posts/posts-to-posts.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
@@ -16,16 +17,26 @@ class OSD_P2P {
 	}
 
 	function register_p2p() {
-		add_action( 'p2p_init', array( &$this, 'p2p_swimmer_result' ) );
+		foreach( $this->_p2p as $p2p ) {
+			add_action( 'p2p_init', array( &$this, 'p2p_' . $p2p ) );
+		}
 	}
 
 
 	/* P2P Connections * * * * * * * * * * * * * * * * * * */
 
-	function p2p_swimmer_result() {
+	function p2p_swimmers_results() {
 		p2p_register_connection_type( array(
 			'name' => 'swimmers_results',
 			'from' => 'swimmer',
+			'to' => 'result'
+		) );
+	}
+
+	function p2p_meetings_results() {
+		p2p_register_connection_type( array(
+			'name' => 'meetings_results',
+			'from' => 'meeting',
 			'to' => 'result'
 		) );
 	}
