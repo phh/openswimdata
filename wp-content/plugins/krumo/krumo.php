3f5684a2c26f7f4c228d2f1a2a26ad43d6ec8abc
Phh <phh@peytz.dk>
2013-11-22 00:12:30 +0000

    adds krumo plugin

---
+++ wp-content/plugins/krumo/krumo.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Krumo
+Description: Krumo is a replacement tool for print_r() and var_dump() for debugging.
+Version: 1.0
+Author: Patrick Hesselberg
+*/
+
+require 'lib/class-krumo.php';
