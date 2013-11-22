10657b6096ca1bc30b04719a16b829fb9b7c1234
Phh <phh@peytz.dk>
2013-11-22 02:26:15 +0000

    OSD_Crawler_Plugin class

--- wp-content/plugins/openswimdata-crawler/openswimdata-crawler.php
+++ wp-content/plugins/openswimdata-crawler/openswimdata-crawler.php
@@ -6,3 +6,25 @@ Version: 1.0
 Author: Patrick Hesselberg
 */
 
+class OSD_Crawler_Plugin {
+	function __construct() {
+		require plugin_dir_path( __FILE__ ) . 'lib/class-osd-generate-urls.php';
+
+		$this->register_plugin_hooks();
+	}
+
+	function register_plugin_hooks() {
+		register_activation_hook( __FILE__, array( &$this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( &$this, 'deactivate' ) );
+	}
+
+	function activate() {
+		add_option( 'osd_urls', array() );
+		add_option( 'osd_style_urls', array() );
+		add_option( 'osd_base_urls', array() );
+	}
+
+	function deactivate() {}
+}
+
+new OSD_Crawler_Plugin;
