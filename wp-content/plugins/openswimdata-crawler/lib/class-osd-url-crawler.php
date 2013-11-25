b45b024d9a3414f0a984aedaa67ddb1c5d1c4f52
Phh <phh@peytz.dk>
2013-11-25 03:07:02 +0000

    adds get_number_elements(), get_firstplace(), get_number_skip(), save_html() for the new save_urls()

--- wp-content/plugins/openswimdata-crawler/lib/class-osd-url-crawler.php
+++ wp-content/plugins/openswimdata-crawler/lib/class-osd-url-crawler.php
@@ -3,13 +3,26 @@
 class OSD_Url_Crawler {
 	var $url;
 	var $html;
+	var $base;
+	var $last_url;
+	var $number_count = 25;
 
 	const BASE = 'http://www.swimrankings.net/index.php?page=rankingDetail&language=us&rankingClubId=';
 
-	function set_url( $url ) {
-		$url = $this::BASE . $url;
+	function set_url( $url, $base = true ) {
+		if( $base ) {
+			$url = $this::BASE . $url;
+		}
 
 		$this->url = $url;
+	}
+
+	function set_base( $base ) {
+		$this->base = $base;
+	}
+
+	function set_last_url( $last_url ) {
+		$this->last_url = $last_url;
 	}
 
 	function request() {
@@ -31,14 +44,58 @@ class OSD_Url_Crawler {
 	function get_urls() {
 		$urls = array();
 
-		$elements = $this->html->find( 'table.navigation td[class=navigation]', -1 )->innertext;
-		$elements = (int) end( explode( ' ', $elements ) );
+		$elements = $this->get_number_elements();
 
 		for( $i = 1; $i <= $elements; $i += 25 ) {
 			$urls[] = $this->url . '&firstPlace=' . $i;
 		}
 
 		return $urls;
+	}
+
+	function get_number_elements() {
+		$elements = $this->html->find( 'table.navigation td[class=navigation]', -1 )->innertext;
+		$elements = (int) end( explode( ' ', $elements ) );
+
+		return $elements;
+	}
+
+	function get_firstplace() {
+		parse_str( $this->url, $parsed );
+
+		return $parsed['firstPlace'];
+	}
+
+	function get_number_skip() {
+		$url_firstplace = $this->get_firstplace();
+		$real_firstplace = $this->html->find( 'input[name=firstPlace]', 0 )->attr['value'];
+		$skip = $url_firstplace - $real_firstplace;
+
+		return $skip;
+	}
+
+	function save_html( $element ) {
+		$html = $this->html->find( $element, -1 )->outertext;
+
+		if( empty( $html ) ) {
+			return false;
+		}
+		$postarr = array(
+			'post_title' => $this->url,
+			'post_type' => 'tmp',
+			'post_status' => 'draft',
+			'post_author' => 1
+		);
+		$tmp = wp_insert_post( $postarr );
+
+		add_post_meta( $tmp, 'tmp_data', $html );
+		add_post_meta( $tmp, 'tmp_base', $this->base );
+
+		if( $this->last_url == $this->url ) {
+			add_post_meta( $tmp, 'tmp_skip', $this->get_number_skip() );
+		}
+
+		return true;
 	}
 }
 
