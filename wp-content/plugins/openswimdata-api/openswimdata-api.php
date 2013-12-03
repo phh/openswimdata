5558026c55af34b9332772dada0f8cd3f06c6332
phh <phh@peytz.dk>
2013-12-03 00:23:00 +0000

    split classes into files

--- wp-content/plugins/openswimdata-api/openswimdata-api.php
+++ wp-content/plugins/openswimdata-api/openswimdata-api.php
@@ -6,21 +6,15 @@ Version: 1.0
 Author: Patrick Hesselberg
 */
 
-function osd_api() {
-	global $osd_api, $wp_json_server;
+include_once( dirname( __FILE__ ) . '/lib/class-osd-api-swimmers.php' );
+include_once( dirname( __FILE__ ) . '/lib/class-osd-api-results.php' );
+include_once( dirname( __FILE__ ) . '/lib/class-osd-api-meetings.php' );
 
-	$osd_api = new OSD_API($wp_json_server);
-	add_filter( 'json_endpoints', array( $osd_api, 'register_routes' ) );
+function osd_api() {
+	global $wp_json_server;
+
+	$swimmers = new OSD_API_Swimmers($wp_json_server);
+	add_filter( 'json_endpoints', array( $swimmers, 'register_routes' ) );
 }
 add_action( 'wp_json_server_before_serve', 'osd_api' );
 
-class OSD_API extends WP_JSON_CustomPostType {
-	protected $base = '/swimmers';
-	protected $type = 'swimmer';
-
-	function register_routes( $routes ) {
-		$routes = parent::registerRoutes( $routes );
-
-		return $routes;
-	}
-}
