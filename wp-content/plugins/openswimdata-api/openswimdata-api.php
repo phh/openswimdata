70ef9bf29f45cbef8bdef5f69bfffef57ec4a348
phh <phh@peytz.dk>
2013-12-04 02:10:07 +0000

    includes osd-api class and osd-api-taxonomies

--- wp-content/plugins/openswimdata-api/openswimdata-api.php
+++ wp-content/plugins/openswimdata-api/openswimdata-api.php
@@ -6,9 +6,11 @@ Version: 1.0
 Author: Patrick Hesselberg
 */
 
+include_once( dirname( __FILE__ ) . '/lib/class-osd-api.php' );
 include_once( dirname( __FILE__ ) . '/lib/class-osd-api-swimmers.php' );
 include_once( dirname( __FILE__ ) . '/lib/class-osd-api-results.php' );
 include_once( dirname( __FILE__ ) . '/lib/class-osd-api-meetings.php' );
+include_once( dirname( __FILE__ ) . '/lib/class-osd-api-taxonomies.php' );
 
 function osd_api() {
 	global $wp_json_server;
@@ -16,6 +18,7 @@ function osd_api() {
 	$swimmers = new OSD_API_Swimmers( $wp_json_server );
 	$results = new OSD_API_Results( $wp_json_server );
 	$meetings = new OSD_API_Meetings( $wp_json_server );
+	$taxonomies = new OSD_API_Taxonomies;
 
 	add_filter( 'json_endpoints', array( $swimmers, 'register_routes' ) );
 	add_filter( 'json_endpoints', array( $results, 'register_routes' ) );
