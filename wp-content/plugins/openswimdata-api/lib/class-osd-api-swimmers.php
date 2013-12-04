5abeef5c7897f21d7e72ae2b407e9c1d68493523
phh <phh@peytz.dk>
2013-12-04 05:56:28 +0000

    removes unused club_date() and adds swimmers_results route

--- wp-content/plugins/openswimdata-api/lib/class-osd-api-swimmers.php
+++ wp-content/plugins/openswimdata-api/lib/class-osd-api-swimmers.php
@@ -13,23 +13,15 @@ class OSD_API_Swimmers extends OSD_API {
 	function register_routes( $routes ) {
 		$routes = parent::registerRoutes( $routes );
 
+		$routes[$this->base . '/results/(?P<id>\d+)' ] = array(
+			array( array( $this, 'connected_get_results' ), WP_JSON_Server::READABLE )
+		);
+
 		return $routes;
 	}
 
-	function club_data( $post ) {
-		$club_data = $this->post_meta( $post['ID'], 'club_data' );
-
-		if( empty( $club_data ) ) {
-			return '';
-		}
-
-		$clubs = array();
-		foreach( $club_data as $club => $data ) {
-			$clubs[$club]['first'] = date( 'd-m-Y', $data['first'] );
-			$clubs[$club]['last'] = date( 'd-m-Y', $data['last'] );
-		}
-
-		return $clubs;
+	function connected_get_results( $id, $context = 'view', $type = 'swimmers_results' ) {
+		return parent::connected_get_results( $id, $context, $type );
 	}
 
 	function json_prepare_meta_club_data( $metas ) {
@@ -53,3 +45,4 @@ class OSD_API_Swimmers extends OSD_API {
 		return $metas;
 	}
 }
+
