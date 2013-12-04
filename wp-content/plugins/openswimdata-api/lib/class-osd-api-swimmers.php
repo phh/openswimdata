33288f91eee6307986d047e4ea6284eafb70509a
phh <phh@peytz.dk>
2013-12-04 04:04:11 +0000

    adds json_prepare_meta_club_data for club data

--- wp-content/plugins/openswimdata-api/lib/class-osd-api-swimmers.php
+++ wp-content/plugins/openswimdata-api/lib/class-osd-api-swimmers.php
@@ -4,6 +4,12 @@ class OSD_API_Swimmers extends OSD_API {
 	protected $base = '/swimmers';
 	protected $type = 'swimmer';
 
+	function __construct( $server ) {
+		parent::__construct( $server );
+
+		add_filter( 'json_prepare_meta', array( &$this, 'json_prepare_meta_club_data' ) );
+	}
+
 	function register_routes( $routes ) {
 		$routes = parent::registerRoutes( $routes );
 
@@ -24,5 +30,26 @@ class OSD_API_Swimmers extends OSD_API {
 		}
 
 		return $clubs;
+	}
+
+	function json_prepare_meta_club_data( $metas ) {
+		if( array_key_exists( 'club_data', $metas ) ) {
+			$this->test_data();
+
+			foreach( $metas['club_data'] as $key => $clubs ) {
+				$clubs = maybe_unserialize( $clubs );
+
+				foreach( $clubs as $club => $data ) {
+					$clubs[$club] = array(
+						'first' => date( 'd-m-Y', $data['first'] ),
+						'last' => date( 'd-m-Y', $data['last'] )
+					);
+				}
+
+				$metas['club_data'][$key] = $clubs;
+			}
+		}
+
+		return $metas;
 	}
 }
