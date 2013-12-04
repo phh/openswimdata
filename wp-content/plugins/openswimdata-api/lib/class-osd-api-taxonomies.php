505135ebfa4e7141b329340616acaa424496f385
phh <phh@peytz.dk>
2013-12-04 04:35:41 +0000

    show terms meta as raw text instead of arrays

--- wp-content/plugins/openswimdata-api/lib/class-osd-api-taxonomies.php
+++ wp-content/plugins/openswimdata-api/lib/class-osd-api-taxonomies.php
@@ -4,12 +4,26 @@ class OSD_API_Taxonomies extends WP_JSON_Taxonomies {
 	function __construct() {
 		global $wp_json_taxonomies;
 
-		remove_filter( 'json_endpoints',      array( $wp_json_taxonomies, 'registerRoutes' ), 2 );
-		remove_filter( 'json_post_type_data', array( $wp_json_taxonomies, 'add_taxonomy_data' ), 10, 2 );
-		remove_filter( 'json_prepare_post',   array( $wp_json_taxonomies, 'add_term_data' ), 10, 3 );
+		remove_filter( 'json_prepare_post', array( $wp_json_taxonomies, 'add_term_data' ), 10, 3 );
+		add_filter( 'json_prepare_post', array( &$this, 'add_term_data' ), 10, 3 );
+	}
 
-		#add_filter( 'json_endpoints',      array( &$this, 'registerRoutes' ), 2 );
-		#add_filter( 'json_post_type_data', array( &$this, 'add_taxonomy_data' ), 10, 2 );
-		#add_filter( 'json_prepare_post',   array( &$this, 'add_term_data' ), 10, 3 );
+	function add_term_data( $data, $post, $context ) {
+		$data = parent::add_term_data( $data, $post, $context );
+		$terms = array();
+		ksort( $data['terms'] );
+
+		foreach( $data['terms'] as $name => $term ) {
+			$term = current( $term );
+			$data[$name] = $term['name'];
+		}
+
+		$meta = $data['meta'];
+		unset( $data['meta'] );
+		unset( $data['terms'] );
+		$data['meta'] = $meta;
+
+		return $data;
 	}
 }
+
