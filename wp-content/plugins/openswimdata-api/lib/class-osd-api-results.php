e862abff66e44833ff7d5b1e2b4a724eb876b179
phh <phh@peytz.dk>
2013-12-04 02:09:05 +0000

    extends OSD_API instead of WP_JSON_CustomPostType. Filters osd_api_results_link and adds p2p links

--- wp-content/plugins/openswimdata-api/lib/class-osd-api-results.php
+++ wp-content/plugins/openswimdata-api/lib/class-osd-api-results.php
@@ -1,12 +1,27 @@
 <?php
 
-class OSD_API_Results extends WP_JSON_CustomPostType {
+class OSD_API_Results extends OSD_API {
 	protected $base = '/results';
 	protected $type = 'result';
+
+	function __construct( $server ) {
+		parent::__construct( $server );
+
+		add_filter( 'osd_api_' . $this->type . '_links', array( &$this, 'osd_api_result_link' ), 10, 2 );
+	}
 
 	function register_routes( $routes ) {
 		$routes = parent::registerRoutes( $routes );
 
 		return $routes;
+	}
+
+	function osd_api_result_link( $links, $post_id ) {
+		$links['swimmer'] = $this->get_related( 'swimmers_results', $post_id, 'swimmers/' );
+		$links['swimmer_results'] = $this->get_related( 'swimmers_results', $post_id, 'swimmers/results/' );
+		$links['meeting'] = $this->get_related( 'meetings_results', $post_id, 'meetings/' );
+		$links['meeting_results'] = $this->get_related( 'meetings_results', $post_id, 'meetings/results/' );
+
+		return $links;
 	}
 }
