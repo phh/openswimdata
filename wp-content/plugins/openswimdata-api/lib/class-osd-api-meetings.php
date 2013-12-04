7bec173e1b9b4370ef4a654ec2cf00735fd68584
phh <phh@peytz.dk>
2013-12-04 05:55:29 +0000

    adds meetings_results route

--- wp-content/plugins/openswimdata-api/lib/class-osd-api-meetings.php
+++ wp-content/plugins/openswimdata-api/lib/class-osd-api-meetings.php
@@ -7,6 +7,15 @@ class OSD_API_Meetings extends OSD_API {
 	function register_routes( $routes ) {
 		$routes = parent::registerRoutes( $routes );
 
+		$routes[$this->base . '/results/(?P<id>\d+)' ] = array(
+			array( array( $this, 'connected_get_results' ), WP_JSON_Server::READABLE )
+		);
+
 		return $routes;
 	}
+
+	function connected_get_results( $id, $context = 'view', $type = 'meetings_results' ) {
+		return parent::connected_get_results( $id, $context, $type );
+	}
 }
+
