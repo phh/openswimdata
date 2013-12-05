c058eb562cbd69fe66d83590bce392c375e69895
phh <phh@peytz.dk>
2013-12-05 01:33:01 +0000

    adds templates for the report

---
+++ wp-content/themes/wordpress-bootstrap/page-rapport.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Template Name: Rapport Page
+*/
+
+query_posts( array(
+	'post_type' => 'page',
+	'posts_per_page' => 50,
+	'order' => 'ASC',
+	'orderby' => 'menu_order',
+	'post__not_in' => array( get_the_ID() )
+) );
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content" class="clearfix row-fluid">
+			
+				<div id="main" class="span12 clearfix" role="main">
+
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					
+					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+					
+						<section class="post_content">
+							<?php the_content(); ?>
+					
+						</section> <!-- end article section -->
+						
+						<footer>
+			
+							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","bonestheme") . ': ', ', ', '</span>'); ?></p>
+							
+						</footer> <!-- end article footer -->
+					
+					</article> <!-- end article -->
+					
+					<?php comments_template(); ?>
+					
+					<?php endwhile; ?>	
+					
+					<?php else : ?>
+					
+					<article id="post-not-found">
+					    <header>
+					    	<h1><?php _e("Not Found", "bonestheme"); ?></h1>
+					    </header>
+					    <section class="post_content">
+					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "bonestheme"); ?></p>
+					    </section>
+					    <footer>
+					    </footer>
+					</article>
+					
+					<?php endif; ?>
+			
+				</div> <!-- end #main -->
+    
+				<?php //get_sidebar(); // sidebar 1 ?>
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
